6b6697ccba32e4b0ed06de4b73f9ccc67e27a40a
Matias Risco <matiasriscolagos@gmail.com>
2023-09-22 12:05:10 +0200

    Add animalA and animalB

---
+++ index.js
@@ -0,0 +1,12 @@
+const animalA = {
+  name: "Giorno Jovana",
+  age: 16,
+  species: "Mono humano",
+  yearOfExtinction: null,
+};
+const animalB = {
+  name: "veigo",
+  age: 5,
+  species: "Dodo",
+  yearOfExtinction: 1662,
+};
