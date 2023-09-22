bd7e667d0e06f46319d6072b23b83183e1d5665c
Matias Risco <matiasriscolagos@gmail.com>
2023-09-22 12:13:13 +0200

    Add getSpeciesOfAnimal

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const animalA = {
+  id: "071A",
   name: "Giorno Jovana",
   age: 16,
   species: "Mono humano",
   yearOfExtinction: null,
 };
 const animalB = {
+  id: "045G",
   name: "veigo",
   age: 5,
   species: "Dodo",
   yearOfExtinction: 1662,
 };
+
+const getSpeciesOfAnimal = (animal) => animal.species;
